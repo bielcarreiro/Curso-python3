6dc02f313bcde937df1ae483956c2f6ecdbbf39f
Gabriel <82422115+bielcarreiro@users.noreply.github.com>
2024-10-21 23:34:20 -0300

    Add files via upload

---
+++ aula08a.py
@@ -0,0 +1,4 @@
+from math import sqrt, ceil, floor
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+print(f'A raiz de {num} é {ceil(raiz)}.')
---
+++ aula08c.py
@@ -0,0 +1,3 @@
+import emoji
+print(emoji.emojize('Hello, World!:globe_showing_Americas:'))
+print(emoji.emojize('Python é irado!:thumbs_up:'))
